1c254ecd24b6142f065d6773a03907c3f1e4cc11
Arman Khan <MY_NAME@example.com>
2022-10-11 15:47:19 +0600

    What purpose is React Router DOM used for question added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,10 +5,10 @@ const Blog = () => {
         <div className='md:w-3/4 mx-auto'>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
         <div className="collapse-title text-xl font-medium">
-          Focus me to see content
+        What purpose is React Router DOM used for?
         </div>
         <div className="collapse-content"> 
-          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          <p>React Router DOM is an npm package that enables we to implement dynamic routing in a web app. It allows we to display pages and allow users to navigate them. It is a fully-featured client and server-side routing library for React. React Router Dom is used to build single-page applications i.e. applications that have many pages or components but the page is never refreshed instead the content is dynamically fetched based on the URL. This process is called Routing and it is made possible with the help of React Router Dom.</p>
         </div>
       </div>
         </div>
