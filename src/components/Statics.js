cc7d5ed7c86825a2f03e934397e8b9db47a7219c
Arman Khan <MY_NAME@example.com>
2022-10-11 19:49:55 +0600

    readme file edited

--- src/components/Statics.js
+++ src/components/Statics.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 const Statics = () => {
     const quizStatics = useLoaderData().data
 
     return (
-        <div className='flex justify-center mt-12'>
-         <LineChart width={600} height={300} data={quizStatics} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <div  style={{maxWidth:'90%', height: 300 }} className='flex justify-center mt-12 mx-auto'>
+        <ResponsiveContainer>
+        <LineChart data={quizStatics} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
     <Line type="monotone" dataKey="total" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="name" />
     <YAxis />
     <Tooltip />
   </LineChart>
+        </ResponsiveContainer>
         </div>
     );
 };
